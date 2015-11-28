419fb35390dfe289d2477ee3823cff9bd11534a8
node-rookie <749924911@qq.com>
2015-11-28 20:13:25 +0800

    update

--- test/Agent/Agent.js
+++ test/Agent/Agent.js
@@ -4,23 +4,37 @@ var assert = require('chai').assert;
 var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
 var open = require('amqplib').connect(url);
 
-describe('agent broker test', function(){
-    it('send and on agent heartbeat', function(done){
-        var brokerPromise = BrokerFactory.create(open, {agent: true, bot: true});
+describe('bot broker test', function(){
+    it('send start agent request', function(done){
+        var brokerPromise = BrokerFactory.create(open, {nm: true, agent: true});
         brokerPromise.then(function(broker){
             var agentBroker = broker.getAgent();
-            var botBroker = broker.getBot();
-            botBroker.onHeartBeat(function(err, beatInfo){
-                console.log(beatInfo);
+            var nodeManagerBroker = broker.getNodeManager();
+            agentBroker.onStartAgent(function(err, startInfo){
+                console.log(startInfo);
             })
-            agentBroker.heartBeat({beat: 'i am a agent heart beat'});
+            nodeManagerBroker.startAgent({info: 'nm start agent'});
+            setTimeout(function(){
+                done();
+            }, 3000);
+        });
+    });
+
+    it('send agent status change request', function(done){
+        var brokerPromise = BrokerFactory.create(open, {bot: true, agent: true});
+        brokerPromise.then(function(broker){
+            var botBroker = broker.getBot();
+            var agentBroker = broker.getAgent();
+            botBroker.onStatusChange(function(err, changeInfo){
+                console.log(changeInfo);
+            })
+            agentBroker.statusChange({info: 'agent status change'});
             setTimeout(function(){
                 done();
             }, 3000);
         });
     })
 })
-
 
 
 
---
+++ test/NodeManager/NodeManager.js
@@ -0,0 +1,26 @@
+var BrokerFactory = require('../../index');
+var rabbitmq = require('base-settings').rabbitmq;
+var assert = require('chai').assert;
+var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
+var open = require('amqplib').connect(url);
+
+describe('agent broker test', function(){
+    it('send and on agent heartbeat', function(done){
+        var brokerPromise = BrokerFactory.create(open, {agent: true, nm: true});
+        brokerPromise.then(function(broker){
+            var agentBroker = broker.getAgent();
+            var nodeManagerBroker = broker.getNodeManager();
+            nodeManagerBroker.onHeartBeat(function(err, beatInfo){
+                console.log(beatInfo);
+            })
+            agentBroker.heartBeat({beat: 'i am a agent heart beat'});
+            setTimeout(function(){
+                done();
+            }, 3000);
+        });
+    })
+})
+
+
+
+
