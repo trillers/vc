a07b4d15af3758c738487527c7e9bc76ad1cd108
henryleu <henryleu@126.com>
2015-11-28 10:34:20 +0800

    test

--- test/AgentManager.js
+++ test/AgentManager.js
@@ -1,17 +1,20 @@
 var BrokerFactory = require('../index');
-var open = require('amqplib').connect('amqp://localhost');
+var rabbitmq = require('base-settings').rabbitmq;
+var url = 'amqp://' +rabbitmq.username + ':' + rabbitmq.password + '@' + rabbitmq.host + ':' + rabbitmq.port + '/' + rabbitmq.vhost;
+var open = require('amqplib').connect(url);
 var brokerPromise = BrokerFactory.create(open);
 brokerPromise.then(function(broker){
+    console.log(broker);
 });
 
 
-var agentBroker = broker.getAgent();
-var agentManager = broker.getAgentManager();
-var bot = broker.getBot();
-
-agentBroker.heartbeat();
-
-agentBroker.onHeartbeat(function(){});
+//var agentBroker = broker.getAgent();
+//var agentManager = broker.getAgentManager();
+//var bot = broker.getBot();
+//
+//agentBroker.heartbeat();
+//
+//agentBroker.onHeartbeat(function(){});
 
 
 ///**
